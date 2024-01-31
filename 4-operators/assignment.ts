79e9e37f379e10c02fcc891a2335b9766e401f3b
indra120 <indrapranata120@gmail.com>
2024-01-31 12:01:22 +0800

    Assignment operators

---
+++ 4-operators/assignment.ts
@@ -0,0 +1,24 @@
+let randomNumber = 69 // Assignment
+
+// Addition & re-assignment
+randomNumber += 42 // randomNumber = randomNumber + 42
+
+// Substraction & re-assignment
+randomNumber -= 42 // randomNumber = randomNumber - 42
+
+// Multiplication & re-assignment
+randomNumber *= 2 // randomNumber = randomNumber * 2
+
+// Division & re-assignment
+randomNumber /= 2 // randomNumber = randomNumber / 2
+
+// Modulo & re-assignment
+randomNumber %= 5 // randomNumber = randomNumber % 5
+
+// Increment
+randomNumber++ // Postfix
+++randomNumber // Prefix
+
+// Decrement
+randomNumber--
+--randomNumber
