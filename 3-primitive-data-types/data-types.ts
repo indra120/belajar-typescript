93c821e3c54b24e6ba67d75073950531b3a042a6
indra120 <indrapranata120@gmail.com>
2024-01-31 11:31:50 +0800

    Primitive data types

---
+++ 3-primitive-data-types/data-types.ts
@@ -0,0 +1,24 @@
+// Undefined
+let belumDikasihNilai
+
+// Null
+let nilainyaKosong = null
+
+// String
+let siswa1: string = "Indra"
+let siswa2 = String("Fairuz")
+
+// Number
+let usia: number = 20
+let beratBadan = Number(56.5)
+
+// Boolean
+let isActive: boolean = false
+let isMale = Boolean(true)
+
+// Big Integer
+let randomBigNumber: bigint = 69420n
+let anotherBigNumber = BigInt(666n)
+
+// Symbol
+let asterisk: symbol = Symbol("*")
