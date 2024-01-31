7f408cbcbbc3ab03ed4d9cd866b15cb4432ee958
indra120 <indrapranata120@gmail.com>
2024-01-31 15:37:34 +0800

    Break the switch

---
+++ 5. Control Flow/2. Switch Statement/2. break.ts
@@ -0,0 +1,12 @@
+let pilihan = "C"
+
+switch (pilihan) {
+  case "A":
+    console.log("Kamu memilih A")
+    break
+  case "B":
+    console.log("Kamu memilih B")
+    break
+  case "C":
+    console.log("Kamu memilih C")
+}
