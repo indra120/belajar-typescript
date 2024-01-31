1e99c1cc514e432f4739159cf06527181b4e655b
indra120 <indrapranata120@gmail.com>
2024-01-31 14:58:10 +0800

    Else-If Statement

---
+++ 5. Control Flow/1. If-Else Statement/3. else-if.ts
@@ -0,0 +1,11 @@
+let usia = 19
+
+if (usia >= 60) {
+  console.log("Lansia")
+} else if (usia >= 18 && usia < 60) {
+  console.log("Dewasa")
+} else if (usia > 12 && usia < 18) {
+  console.log("Remaja")
+} else {
+  console.log("Anak-anak")
+}
