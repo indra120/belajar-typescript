e9cf7a0ae0d81f30bb04efbd2c0fcabfd4f313b0
indra120 <indrapranata120@gmail.com>
2024-01-31 12:22:32 +0800

    Logical comparison operators

---
+++ 4-operators/logical-comparison.ts
@@ -0,0 +1,12 @@
+// AND
+console.log(69 < 70 && 420 > 69)
+console.log(69 > 70 && 420 > 69)
+
+// OR
+console.log(69 < 70 || 420 > 69)
+console.log(69 > 70 || 420 > 69)
+console.log(69 > 70 || 420 < 69)
+
+// NOT
+console.log(!true)
+console.log(!false)
