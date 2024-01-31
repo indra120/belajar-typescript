0a6bae91296e01afe8acbe09908b33901bd057af
indra120 <indrapranata120@gmail.com>
2024-01-31 10:46:23 +0800

    Variable Declaration

---
+++ 2-variables/declarations.ts
@@ -0,0 +1,14 @@
+let nama: string = "Indra"
+console.log(nama)
+
+nama = "Fairuz"
+console.log(nama)
+
+let usia: number
+usia = 19
+
+console.log(usia)
+
+const gender = "laki-laki"
+// gender = "perempuan" // Error
+console.log(gender)
