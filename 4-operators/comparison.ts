c73e9371cc149bab86fdda210432824e7447d599
indra120 <indrapranata120@gmail.com>
2024-01-31 12:16:17 +0800

    Comparison operators

---
+++ 4-operators/comparison.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+let nice = 69
+
+console.log('nice == "69" : ', nice == "69")
+console.log('nice === "69" : ', nice === "69")
+console.log('nice != "69" : ', nice != "69")
+console.log('nice !== "69" : ', nice !== "69")
+
+console.log("nice > 69 : ", nice > 69)
+console.log("nice < 69 : ", nice < 69)
+console.log("nice >= 69 : ", nice >= 69)
+console.log("nice <= 69 : ", nice <= 69)
