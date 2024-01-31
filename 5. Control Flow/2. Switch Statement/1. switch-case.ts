55c3ab7162c4d9586bb1094c9c93a93ab5ed7b22
indra120 <indrapranata120@gmail.com>
2024-01-31 15:33:22 +0800

    Switch-Case Statement

---
+++ 5. Control Flow/2. Switch Statement/1. switch-case.ts
@@ -0,0 +1,10 @@
+let pilihan = "A"
+
+switch (pilihan) {
+  case "A":
+    console.log("Kamu memilih A")
+  case "B":
+    console.log("Kamu memilih B")
+  case "C":
+    console.log("Kamu memilih C")
+}
