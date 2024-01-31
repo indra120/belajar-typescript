cf56ae6184c2156fcda939f8adc4fc6592bce068
indra120 <indrapranata120@gmail.com>
2024-01-31 19:36:02 +0800

    Function Declaration

---
+++ 6. Functions/1. declaration.ts
@@ -0,0 +1,12 @@
+function helloWorld(): void {
+  console.log("Hello world!")
+}
+helloWorld()
+helloWorld()
+
+function hiMom(): string {
+  return "Hi Mom!"
+}
+
+const greeting = hiMom()
+console.log(greeting)
