7a29ca8f8fa372f140db01de42ddc3948fb99a21
indra120 <indrapranata120@gmail.com>
2024-01-31 12:00:56 +0800

    Arithmetic operator

---
+++ 4-operators/arithmetic.ts
@@ -0,0 +1,5 @@
+console.log(22 + 47) // Addition
+console.log(100 - 31) // Substraction
+console.log(23 * 3) // Multiplication
+console.log(138 / 2) // Division
+console.log(209 % 3) // Modulo (remainder of division)
